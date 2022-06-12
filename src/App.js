dc5bca4d2890924130447f472b09dc993b1169bc
Niyaz <macbook@MacBook-Air.local>
2022-06-12 17:35:57 +0500

    фильнальные изменения

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import './App.css';
 import {HashRouter, Routes, Route} from "react-router-dom";
 import {LandingPage} from "./page";
 
-const guestList = [
-  "Максим и Дарья",
-  "Лена и Рома",
-  "Александр и Анастасия",
-  "Алексей и Вироника",
-  "Владимир и Аня",
+const TextForGuests = [
+"Это незабываемое событие мы хотим отметить в кругу самых близких для нас людей. Приглашаем разделить этот счастливый день вместе с нами!",
+"Да, вы правы, все пропало! Она смогла меня охомутать, у нее получилось! Спасайте! SOS! Своего пропавшего друга вы легко узнаете – я буду в черном костюме, а захватчик в шикарном белом платье. Посмеяться над моей загубленной холостяцкой свободной жизнью вы сможете в «Банкетном доме» по адресу ул. Советская, 104. Приходите обязательно, всем будет весело, погибать будем с музыкой!"
 ]
 
 function App() {
-  const generatePages = guestList.map((item, index) => {
+  const generatePages = TextForGuests.map((text, index) => {
     return (
-      <Route key={index} path={`/${index}`} element={<LandingPage guestNames={item} />} />
+      <Route key={index} path={`/${index}`} element={<LandingPage text={text} />} />
     )
   })
 
   return (
     <HashRouter>
         <Routes>
-          <Route path='/' element={<LandingPage guestNames="Максим и Дарья" />}/>
           {generatePages}
         </Routes>
     </HashRouter>
--- src/page.jsx
+++ src/page.jsx
@@ -1,14 +1,13 @@
 import { FormApprove } from './form';
 
-export const LandingPage = ({guestNames}) => {
-    console.log(guestNames);
+export const LandingPage = ({text}) => {
+
     return (
         <div className="App">
           <header className="App-header">
             <div className="content">
-              <h1>{guestNames}</h1>
-              <p className="description">Нам очень хочется, чтобы в этот счастливый момент нас окружали самые дорогие и близкие люди, а это значит, что мы будем рады видеть Вас на торжестве, посвященном созданию нашей Семьи</p>
-              <p className="place">которое состоится: <br /><b> 3 июля 2022 года</b></p>
+              <h1>Дорогие наши и любимые! <br /> Мы женимся!</h1>
+              <div className='description'>{text}</div>
     
               <div className="arrow-down" />
             </div>
@@ -16,31 +15,36 @@ export const LandingPage = ({guestNames}) => {
     
           <section className="programm-day">
             <h2>ПРОГРАММА ДНЯ</h2>
-            <div className="subtitle">Пятница, 3 Июля</div>
+            <div className="subtitle">Пятница, 19 августа 2022 года</div>
     
             <ul className="programm-list">
               <li>
-                <span className="time">16 : 00</span>
+                <span className="time">17 : 00</span>
                 <span className="image" />
                 <span className="description">Сбор гостей</span>
               </li>
               <li>
-                <span className="time">16 : 30</span>
+                <span className="time">17 : 30</span>
                 <span className="image" />
                 <span className="description">Церемония бракосочетания</span>
               </li>
               <li>
-                <span className="time">17 : 00</span>
+                <span className="time">18 : 00</span>
                 <span className="image" />
                 <span className="description">Торжественный банкет</span>
+              </li>
+              <li>
+                <span className="time">00 : 00</span>
+                <span className="image" />
+                <span className="description">Окончание мероприятия </span>
               </li>
             </ul>
           </section>
     
           <section className='place'>
             <h2>МЕСТО ПРОВЕДЕНИЯ</h2>
-            <div className="subtitle">Свою свадьбу мы решили отметить по адресу: с.Верх-Иньва, ул.Восточная, д.20 <br /> Мы подготовили для Вас карту, что бы Вы точно добрались и порадовали нас своим присутствием</div>
-            <iframe className='place-map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d976.7571150774221!2d56.23383274344146!3d58.01644214596169!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43e8c72090293005%3A0xe9f8205e1cd8da5e!2z0KHQutCy0LXRgCDQvdCwINCh0L7QsdC-0YDQvdC-0Lkg0L_Qu9C-0YnQsNC00Lg!5e0!3m2!1sru!2sru!4v1653893984265!5m2!1sru!2sru" height="450" style={{border : 0, borderRadius: "4px"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+            <div className="subtitle">Свою свадьбу мы решили отметить по адресу: г. Пермь, ул. Советская, 104, «Банкетный дом» <br /> Мы подготовили для Вас карту, что бы Вы точно добрались и порадовали нас своим присутствием</div>
+            <iframe className='place-map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2113.660699480921!2d56.21150681601941!3d58.010513281216895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43e8c78cc22fdd6b%3A0x6cd0465af0c56190!2z0JHQsNC90LrQtdGC0L3Ri9C5INC00L7QvA!5e0!3m2!1sru!2sru!4v1655037110231!5m2!1sru!2sru" height="450" style={{border : 0, borderRadius: "4px"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </section>
     
           <section className='form-approve'>
